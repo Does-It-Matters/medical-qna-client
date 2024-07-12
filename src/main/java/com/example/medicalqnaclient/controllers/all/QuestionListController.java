dd55fff924bf8ad5083aeb8198d5177712b4bc7c
glass <silver20190204@gmail.com>
2024-07-12 22:23:23 +0900

    Add combobox items

--- src/main/java/com/example/medicalqnaclient/controllers/all/QuestionListController.java
+++ src/main/java/com/example/medicalqnaclient/controllers/all/QuestionListController.java
@@ -1,11 +1,29 @@
 package com.example.medicalqnaclient.controllers.all;
 
 import com.example.medicalqnaclient.HelloApplication;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
 import java.io.IOException;
+import java.util.List;
 
 public class QuestionListController {
+
+    @FXML
+    private ComboBox<String> categoryComboBox;
+
+    @FXML
+    protected void initialize() {
+        populateCategoryComboBox();
+    }
+
+    private void populateCategoryComboBox() {
+        List<String> categories = List.of("Health", "Medicine", "Lifestyle", "Fitness");
+        ObservableList<String> observableCategories = FXCollections.observableArrayList(categories);
+        categoryComboBox.setItems(observableCategories);
+    }
 
     @FXML
     protected void onLoginButtonClick() {
