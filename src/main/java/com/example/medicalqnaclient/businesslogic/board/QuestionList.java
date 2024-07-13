1afa21d832dc38c20c440c295fcb8774917409f5
glass <silver20190204@gmail.com>
2024-07-13 17:47:36 +0900

    Fix single event listener (Issue #3)

--- src/main/java/com/example/medicalqnaclient/businesslogic/board/QuestionList.java
+++ src/main/java/com/example/medicalqnaclient/businesslogic/board/QuestionList.java
@@ -19,18 +19,18 @@ public class QuestionList implements Observer {
         this.listView = listView;
         this.category = category;
         this.category.add(this);
-    }
-
-    public void setList(List<Question> questions) {
-        ObservableList<Question> items = FXCollections.observableArrayList(questions);
-        listView.setItems(items);
-//        Platform.runLater(() -> listView.setItems(items)); // 백그라운드 스레드 실행을 막기 위해, UI 업데이트를 JavaFX 스레드에서 실행하는 것을 보장
 
         listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
                 handleSelectedQuestion(newValue);
             }
         });
+    }
+
+    public void setList(List<Question> questions) {
+        ObservableList<Question> items = FXCollections.observableArrayList(questions);
+        listView.setItems(items);
+//        Platform.runLater(() -> listView.setItems(items)); // 백그라운드 스레드 실행을 막기 위해, UI 업데이트를 JavaFX 스레드에서 실행하는 것을 보장
     }
 
     private void handleSelectedQuestion(Question question) {
