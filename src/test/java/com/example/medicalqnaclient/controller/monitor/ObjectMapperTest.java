8590a3866046add924558e6a41cc0c79fbc512d6
glass <silver20190204@gmail.com>
2024-08-05 03:46:09 +0900

    Add junit test case (Issue #59, #61)

---
+++ src/test/java/com/example/medicalqnaclient/controller/monitor/ObjectMapperTest.java
@@ -0,0 +1,22 @@
+package com.example.medicalqnaclient.controller.monitor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ObjectMapperTest {
+    @Test
+    public void testSerialization() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        CpuStatDTO dto = new CpuStatDTO();
+        dto.putSingleCoreStat("cpu0", List.of(100, 200, 300, 400));
+
+        String json = objectMapper.writeValueAsString(dto);
+        CpuStatDTO deserializedDto = objectMapper.readValue(json, CpuStatDTO.class);
+
+        assertEquals(dto.getCpuStats(), deserializedDto.getCpuStats());
+    }
+}
