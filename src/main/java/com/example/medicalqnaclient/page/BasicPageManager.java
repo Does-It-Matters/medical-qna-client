c6eab3cb5cefdf8dbde0337add4f7a078575ae2d
glass <silver20190204@gmail.com>
2024-08-12 02:10:03 +0900

    Add a concrete page manager and page interface (Issue #68)

---
+++ src/main/java/com/example/medicalqnaclient/page/BasicPageManager.java
@@ -0,0 +1,34 @@
+package com.example.medicalqnaclient.page;
+
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BasicPageManager implements PageManager {
+    private final static Map<PageType, Page> pages;
+
+    static {
+        pages = new HashMap<>();
+    }
+
+    private Stage stage;
+
+    public BasicPageManager (Stage stage, String title) {
+        this.stage = stage;
+        stage.setTitle(title);
+    }
+
+    @Override
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    @Override
+    public void show(PageType type) {
+        Page page = pages.get(type);
+        stage.setScene(page.getScene());
+        stage.setTitle(page.getTitle());
+        stage.show();
+    }
+}
---
+++ src/main/java/com/example/medicalqnaclient/page/Page.java
@@ -0,0 +1,8 @@
+package com.example.medicalqnaclient.page;
+
+import javafx.scene.Scene;
+
+public interface Page {
+    Scene getScene();
+    String getTitle();
+}
