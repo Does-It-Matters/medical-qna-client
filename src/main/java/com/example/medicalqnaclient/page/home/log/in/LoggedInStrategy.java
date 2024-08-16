0c0ea09e405894300a9ee131781b87a1e715eb38
glass <silver20190204@gmail.com>
2024-08-16 23:21:16 +0900

    스프링 빈 등록

--- src/main/java/com/example/medicalqnaclient/page/home/log/in/LoggedInStrategy.java
+++ src/main/java/com/example/medicalqnaclient/page/home/log/in/LoggedInStrategy.java
@@ -1,9 +1,13 @@
 package com.example.medicalqnaclient.page.home.log.in;
 
 import com.example.medicalqnaclient.page.PageStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LoggedInStrategy extends PageStrategy {
 
+    @Autowired
     public LoggedInStrategy(QuestionsLoggedInUsersController questionsLoggedInUsersController) {
         super(questionsLoggedInUsersController);
     }
--- src/main/java/com/example/medicalqnaclient/page/home/log/out/LoggedOutStrategy.java
+++ src/main/java/com/example/medicalqnaclient/page/home/log/out/LoggedOutStrategy.java
@@ -1,9 +1,13 @@
 package com.example.medicalqnaclient.page.home.log.out;
 
 import com.example.medicalqnaclient.page.PageStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LoggedOutStrategy extends PageStrategy {
 
+    @Autowired
     public LoggedOutStrategy(QuestionsLoggedOutUsersController questionsLoggedOutUsersController) {
         super(questionsLoggedOutUsersController);
     }
