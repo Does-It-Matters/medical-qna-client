d17f3193911e347ed3fd1a484b96764dcac94331
glass <silver20190204@gmail.com>
2024-08-16 21:32:51 +0900

    Add home page 전략 패턴 활용 (Issue #71)

--- src/main/java/com/example/medicalqnaclient/page/qna/HomePage.java
+++ src/main/java/com/example/medicalqnaclient/page/qna/HomePage.java
@@ -2,14 +2,26 @@ package com.example.medicalqnaclient.page.qna;
 
 import com.example.medicalqnaclient.page.AbstractPage;
 import com.example.medicalqnaclient.page.PageStrategy;
+import com.example.medicalqnaclient.user.mediator.UserMediator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HomePage extends AbstractPage {
+    private final UserMediator userMediator;
+    private final PageStrategy loggedInStrategy;
+    private final PageStrategy loggedOutStrategy;
 
     @Autowired
-    public HomePage(PageStrategy strategy) {
-        super("Home Page", strategy);
+    public HomePage(UserMediator userMediator, LoggedInStrategy loggedInStrategy, LoggedOutStrategy loggedOutStrategy) {
+        super("Home Page", loggedInStrategy);
+        this.userMediator = userMediator;
+        this.loggedInStrategy = loggedInStrategy;
+        this.loggedOutStrategy = loggedOutStrategy;
+    }
+
+    @Override
+    protected void updateStrategy() {
+        strategy = (userMediator.isLoggedIn()) ? loggedInStrategy : loggedOutStrategy;
     }
 }
