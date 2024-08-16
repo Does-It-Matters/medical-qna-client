481a1740a59b04564fc307b2055e285de96b9f14
glass <silver20190204@gmail.com>
2024-08-17 00:51:55 +0900

    Apply go home event listener (Issue #76)

--- src/main/java/com/example/medicalqnaclient/page/event/Listener.java
+++ src/main/java/com/example/medicalqnaclient/page/event/Listener.java
@@ -1,13 +1,22 @@
 package com.example.medicalqnaclient.page.event;
 
+import com.example.medicalqnaclient.page.PageManager;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import static com.example.medicalqnaclient.page.PageType.HOME_PAGE;
+
 @Component
 public class Listener {
+    private final PageManager pageManager;
+
+    public Listener(PageManager pageManager) {
+        this.pageManager = pageManager;
+    }
 
     @EventListener
     public void handleEvent(GoHomeEvent event) {
         System.out.println("Handling event: " + event.getMessage());
+        pageManager.show(HOME_PAGE);
     }
 }
