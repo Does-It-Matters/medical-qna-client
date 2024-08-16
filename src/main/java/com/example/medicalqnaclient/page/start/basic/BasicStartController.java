b6c0dc55d385708c9849d3eee26f980f512d0559
glass <silver20190204@gmail.com>
2024-08-16 23:05:21 +0900

    Refactor Start 전략 클래스에서 화면 구성 캡슐화해서 분리

---
+++ src/main/java/com/example/medicalqnaclient/page/start/basic/BasicStartController.java
@@ -0,0 +1,23 @@
+package com.example.medicalqnaclient.page.start.basic;
+
+import com.example.medicalqnaclient.page.ViewController;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BasicStartController implements ViewController {
+    @Override
+    public Pane getLayout() {
+        VBox root = new VBox(10);
+        Button goHomeButton = new Button("Go Home");
+        Button getMonitorPageButton = new Button("Monitor page");
+
+//        goHomeButton.setOnAction(e -> mediator.goHome());
+//        getMonitorPageButton.setOnAction(e -> mediator.getMonitorView());
+
+        root.getChildren().addAll(goHomeButton, getMonitorPageButton);
+        return root;
+    }
+}
--- src/main/java/com/example/medicalqnaclient/page/start/basic/BasicStartStrategy.java
+++ src/main/java/com/example/medicalqnaclient/page/start/basic/BasicStartStrategy.java
@@ -1,32 +1,25 @@
 package com.example.medicalqnaclient.page.start.basic;
 
 import com.example.medicalqnaclient.page.PageStrategy;
+import com.example.medicalqnaclient.page.ViewController;
 import com.example.medicalqnaclient.user.mediator.UserMediator;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BasicStartStrategy implements PageStrategy {
     private final UserMediator userMediator;
+    private final ViewController basicStartController;
 
     @Autowired
-    public BasicStartStrategy(UserMediator userMediator) {
+    public BasicStartStrategy(UserMediator userMediator, BasicStartController basicStartController) {
         this.userMediator = userMediator;
+        this.basicStartController = basicStartController;
     }
 
     @Override
     public Pane createLayout() {
-        VBox root = new VBox(10);
-        Button goHomeButton = new Button("Go Home");
-        Button getMonitorPageButton = new Button("Monitor page");
-
-//        goHomeButton.setOnAction(e -> mediator.goHome());
-//        getMonitorPageButton.setOnAction(e -> mediator.getMonitorView());
-
-        root.getChildren().addAll(goHomeButton, getMonitorPageButton);
-        return root;
+        return basicStartController.getLayout();
     }
 }
