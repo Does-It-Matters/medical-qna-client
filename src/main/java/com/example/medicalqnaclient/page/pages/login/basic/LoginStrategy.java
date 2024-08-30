fc4781999cd95ac081198f08490c8f6efe2ba627
glass <silver20190204@gmail.com>
2024-08-30 19:19:46 +0900

    Add Login 화면 기본 뼈대
    - LoginPage, 로그인 전략을 선택하여 화면 표현
    - LoginController, 로그인 ui 캡슐화
    - LoginStrategy, 컨트롤러로 ui 캡슐화한 로그인 전략

---
+++ src/main/java/com/example/medicalqnaclient/page/pages/login/basic/LoginStrategy.java
@@ -0,0 +1,20 @@
+package com.example.medicalqnaclient.page.pages.login.basic;
+
+import com.example.medicalqnaclient.page.core.PageStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * <b> 역할: 로그인 화면 컨트롤러 전략 클래스 </b>
+ * <p>
+ * - 로그인 화면을 캡슐화하여 위임 <br>
+ * </p>
+ */
+@Component
+public class LoginStrategy extends PageStrategy {
+
+    @Autowired
+    public LoginStrategy(LoginController loginController) {
+        super(loginController);
+    }
+}
