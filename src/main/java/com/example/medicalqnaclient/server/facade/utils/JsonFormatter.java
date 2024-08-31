fe8e564faff4f2d57f574a5808c7db664097ee5c
glass <silver20190204@gmail.com>
2024-08-31 10:31:50 +0900

    Add network util
    - Get, Get 요청
    - Post, Post 요청
    - Patch, Patch 요청
    - Delete, Delete 요청
    - JsonFormatter, json으로 직렬화
    - Response, 상태 코드와 바디로 응답 구성

--- src/main/java/com/example/medicalqnaclient/server/facade/utils/JsonFormatter.java
+++ src/main/java/com/example/medicalqnaclient/server/facade/utils/JsonFormatter.java
@@ -1,19 +1,19 @@
-//package com.example.medicalqnaclient.server.facade.utils;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.ObjectWriter;
-//import com.fasterxml.jackson.databind.SerializationFeature;
-//
-//public class JsonFormatter {
-//    public static String formatJson(Object data) {
-//        try {
-//            ObjectMapper mapper = new ObjectMapper();
-//            mapper.enable(SerializationFeature.INDENT_OUTPUT);
-//            ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
-//            return writer.writeValueAsString(data);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-//    }
-//}
+package com.example.medicalqnaclient.server.facade.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class JsonFormatter {
+    public static String formatJson(Object data) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
+            return writer.writeValueAsString(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
