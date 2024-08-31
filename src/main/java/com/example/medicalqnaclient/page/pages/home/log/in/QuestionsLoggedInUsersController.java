07c5113f6121d5878b283061f22d857a8de21e82
glass <silver20190204@gmail.com>
2024-08-31 18:13:11 +0900

    Add 질문 리스트, 질문 조회

--- src/main/java/com/example/medicalqnaclient/page/pages/home/log/in/QuestionsLoggedInUsersController.java
+++ src/main/java/com/example/medicalqnaclient/page/pages/home/log/in/QuestionsLoggedInUsersController.java
@@ -2,18 +2,22 @@ package com.example.medicalqnaclient.page.pages.home.log.in;
 
 import com.example.medicalqnaclient.page.core.ViewController;
 import com.example.medicalqnaclient.page.event.publishers.QnAPublisher;
+import com.example.medicalqnaclient.server.facade.tasks.questionlist.QuestionTitle;
 import com.example.medicalqnaclient.user.mediator.ReadWriteUserMediator;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class QuestionsLoggedInUsersController extends ViewController {
     private ComboBox<String> categoryComboBox;
-    private ListView<String> listView;
-//    private ListView<QuestionTitle> listView;
+    private ListView<QuestionTitle> listView;
     private MenuButton userNameMenuButton;
     private TextArea userQueryTextArea;
     private Button logoutButton;
@@ -38,38 +42,40 @@ public class QuestionsLoggedInUsersController extends ViewController {
         postButton = new Button("Post");
 
         logoutButton.setOnAction(e -> onLogoutButtonClick());
-//        searchButton.setOnAction(e -> onSearchButtonClick());
+        searchButton.setOnAction(e -> onSearchButtonClick());
         postButton.setOnAction(e -> onPostButtonClick());
 
         layout.getChildren().addAll(userNameMenuButton, categoryComboBox, listView, userQueryTextArea, searchButton, postButton, logoutButton);
-//        setList(mediator.getQuestionList());
+        setList(mediator.getQuestionList());
 
         return layout;
     }
 
-//    private void handleSelectedQuestion(QuestionTitle questionTitle) {
-//        mediator.readQuestion(questionTitle.getQuestionId());
-//    }
 
     protected void onLogoutButtonClick() {
         mediator.logout();
         publisher.publishGoHomeEvent();
     }
-//
-//    protected void onSearchButtonClick() {
-//        setList(mediator.search(userQueryTextArea.getText()));
-//    }
-//
-//    private void setList(List<QuestionTitle> list) {
-//        ObservableList<QuestionTitle> items = FXCollections.observableArrayList(list);
-//        listView.setItems(items);
-//
-//        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-//            if (newValue != null) {
-//                handleSelectedQuestion(newValue);
-//            }
-//        });
-//    }
+
+    protected void onSearchButtonClick() {
+        setList(mediator.search(userQueryTextArea.getText()));
+    }
+
+    private void setList(List<QuestionTitle> list) {
+        ObservableList<QuestionTitle> items = FXCollections.observableArrayList(list);
+        listView.setItems(items);
+
+        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                handleSelectedQuestion(newValue);
+            }
+        });
+    }
+
+    private void handleSelectedQuestion(QuestionTitle questionTitle) {
+        mediator.setReadingQuestionId(questionTitle.getQuestionId());
+        publisher.publishQuestionViewEvent();
+    }
 
     protected void onPostButtonClick() {
         publisher.publishQuestionPostEvent();
