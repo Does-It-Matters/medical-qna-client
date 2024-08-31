75f535075f6d1e6fe53e7a62ece77e7c3fc06444
glass <silver20190204@gmail.com>
2024-08-31 09:55:41 +0900

    Add server 인터페이스

--- src/main/java/com/example/medicalqnaclient/server/facade/AuthService.java
+++ src/main/java/com/example/medicalqnaclient/server/facade/AuthService.java
@@ -1,10 +1,10 @@
-//package com.example.medicalqnaclient.server.facade;
-//
+package com.example.medicalqnaclient.server.facade;
+
 //import com.example.medicalqnaclient.server.facade.tasks.signup.common.SignUpResponse;
 //import com.example.medicalqnaclient.server.facade.tasks.user.login.LoginResponse;
-//
-//public interface AuthService {
+
+public interface AuthService {
 //    LoginResponse login(String id, String password) throws Exception;
 //    SignUpResponse signUpAsPatient(String id, String pw);
 //    SignUpResponse signUpAsDoctor(String id, String pw, String name, String field, String hospital, String introduction);
-//}
+}
