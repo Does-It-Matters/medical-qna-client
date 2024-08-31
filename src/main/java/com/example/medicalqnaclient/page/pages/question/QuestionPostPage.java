160033191f5cfcff310b74ce274dfc46cccc8cf7
glass <silver20190204@gmail.com>
2024-08-31 13:59:12 +0900

    Add other page, strategy

---
+++ src/main/java/com/example/medicalqnaclient/page/pages/question/QuestionPostPage.java
@@ -0,0 +1,37 @@
+package com.example.medicalqnaclient.page.pages.question;
+
+import com.example.medicalqnaclient.page.core.AbstractPage;
+import com.example.medicalqnaclient.page.core.PageStrategy;
+import com.example.medicalqnaclient.page.pages.profile.basic.MyProfileStrategy;
+import com.example.medicalqnaclient.page.pages.question.basic.QuestionPostStrategy;
+import com.example.medicalqnaclient.user.mediator.ReadUserMediator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * <b> 역할: 질문 등록 페이지 클래스 </b>
+ * <p>
+ * - 전략 패턴을 활용하여 화면 구성 <br>
+ * </p>
+ */
+@Component
+public class QuestionPostPage extends AbstractPage {
+    private final PageStrategy questionPostStrategy;
+
+    @Autowired
+    public QuestionPostPage(ReadUserMediator userMediator, QuestionPostStrategy questionPostStrategy) {
+        super("Question Post Page", userMediator, questionPostStrategy);
+        this.questionPostStrategy = questionPostStrategy;
+    }
+
+    /**
+     * <b> 역할: 전략 갱신 메소드 </b>
+     * <p>
+     * - 전략 선택 <br>
+     * </p>
+     */
+    @Override
+    protected void updateStrategy() {
+        strategy = questionPostStrategy;
+    }
+}
---
+++ src/main/java/com/example/medicalqnaclient/page/pages/question/basic/QuestionEditStrategy.java
@@ -0,0 +1,20 @@
+package com.example.medicalqnaclient.page.pages.question.basic;
+
+import com.example.medicalqnaclient.page.core.PageStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * <b> 역할: 질문 수정 화면 컨트롤러 전략 클래스 </b>
+ * <p>
+ * - 질문 수정을 캡슐화하여 위임 <br>
+ * </p>
+ */
+@Component
+public class QuestionEditStrategy extends PageStrategy {
+
+    @Autowired
+    public QuestionEditStrategy(QuestionEditController questionEditController) {
+        super(questionEditController);
+    }
+}
