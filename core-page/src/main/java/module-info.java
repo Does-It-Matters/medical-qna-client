eecdfba181ca8a8c68fc13ad934aab0dc3b30a8d
glass <silver20190204@gmail.com>
2024-09-02 20:07:06 +0900

    Add core-page submodule (Issue #95)

---
+++ core-page/src/main/java/module-info.java
@@ -0,0 +1,25 @@
+module com.example.medicalqnaclient.core.page {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires java.net.http;
+    requires com.fasterxml.jackson.databind;
+    requires rsocket.core;
+    requires rsocket.transport.netty;
+    requires reactor.core;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.boot;
+    requires spring.beans;
+    requires spring.core;
+    requires spring.messaging;
+
+    requires org.slf4j;
+    requires ch.qos.logback.classic;
+    requires spring.web;
+}
